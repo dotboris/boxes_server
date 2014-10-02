e3d34de8c32c41ea96cd808d4aae243ccb236806
Boris Bera <bboris@rsoft.ca>
2014-10-01 21:50:12 -0400

    Added mq host and port options to cli

--- lib/boxes/cli.rb
+++ lib/boxes/cli.rb
@@ -10,6 +10,15 @@ module Boxes
       @banner = "usage: #{name} [options]"
     end
 
+    option :mq_host, long: '--mq-host HOST',
+           description: 'host for the rabbit mq instance',
+           default: 'localhost'
+
+    option :mq_port, long: '--mq-port PORT',
+           desctiption: 'port for the rabbit mq instance',
+           proc: proc {|port| port.to_i},
+           default: 5672
+
     option :version, short: '-v', long: '--version',
            description: 'show version information',
            on: :tail,
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -7,33 +7,61 @@ describe Boxes::Cli do
     allow($stdout).to receive(:write)
   end
 
-  describe 'help' do
-    it 'should exit' do
-      cli = Boxes::Cli.new 'command'
+  let(:cli) { Boxes::Cli.new 'command' }
 
+  describe '--help' do
+    it 'should exit' do
       expect{cli.parse_options(['-h'])}.to raise_error SystemExit
     end
 
     it 'should output banner' do
-      cli = Boxes::Cli.new 'command'
       allow(cli).to receive(:exit)
 
       expect{cli.parse_options(['-h'])}.to output(/^usage: command \[options\]/).to_stdout
     end
   end
 
-  describe 'version' do
+  describe '--version' do
     it 'should exit' do
-      cli = Boxes::Cli.new 'command'
-
       expect{cli.parse_options(['-v'])}.to raise_error SystemExit
     end
 
     it 'should output the version' do
-      cli = Boxes::Cli.new 'command'
       allow(cli).to receive(:exit)
 
       expect{cli.parse_options ['-v']}.to output(Regexp.new Regexp.escape(Boxes::VERSION.to_s)).to_stdout
+    end
+  end
+
+  describe '--mq-host' do
+    it 'should default to localhost' do
+      cli.parse_options []
+
+      expect(cli.config[:mq_host]).to eq('localhost')
+    end
+    it 'should store value in config' do
+      cli.parse_options %w(--mq-host some_host)
+
+      expect(cli.config[:mq_host]).to eq('some_host')
+    end
+  end
+
+  describe '--mq-port' do
+    it 'should default to 5672' do
+      cli.parse_options []
+
+      expect(cli.config[:mq_port]).to eq(5672)
+    end
+    it 'should convert value to int' do
+      cli.parse_options %w(--mq-port 1234)
+
+      expect(cli.config[:mq_port]).to be_kind_of(Integer)
+    end
+
+    it 'should store value in config' do
+      cli.parse_options %w(--mq-port 1234)
+
+      expect(cli.config[:mq_port]).to eq(1234)
     end
   end
 end
