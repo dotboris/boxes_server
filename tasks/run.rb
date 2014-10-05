774ea2337bb543658243efce3894f0f3e2f74409
Boris Bera <bboris@rsoft.ca>
2014-10-05 00:01:43 -0400

    automated running severs

--- tasks/run.rb
+++ tasks/run.rb
@@ -7,4 +7,9 @@ namespace :run do
       sh "docker run -t --rm --link rabbitmq:rabbitmq boxes/#{gem}"
     end
   end
+
+  desc 'Run servers (rabbitmq) as daemons. Old versions will be removed.'
+  task :servers do
+    sh 'docker run -d -P --name rabbitmq -e RABBITMQ_USER=boxes -e RABBITMQ_PASS=boxes tutum/rabbitmq'
+  end
 end
