cad4153cd344ccc1ecd7593ed6f3f449ddefc8e3
Boris Bera <bboris@rsoft.ca>
2014-10-15 11:43:53 -0400

    Removed warning version copy headers

--- scalpel/lib/scalpel/version.rb
+++ scalpel/lib/scalpel/version.rb
@@ -1,5 +1,3 @@
-# WARNING: This file was generate by the scalpel:update_version rake task. To
-# update the version exit the BOXES_VERSION file located in the project root.
 module Scalpel
   VERSION = '0.0.1'
 end
