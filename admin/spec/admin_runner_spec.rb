0094ad0cd63b4281b53454f396b8572eee7a5a57
Boris Bera <bboris@rsoft.ca>
2014-10-15 18:36:08 -0400

    removed pending test for rabbitmq integration.
    This stuff is a bit of a bitch to mock out. There is a gem out there, but it doesn't look properly released.

--- admin/spec/admin_runner_spec.rb
+++ admin/spec/admin_runner_spec.rb
@@ -6,14 +6,16 @@ describe Boxes::AdminRunner do
   let(:runner) { Boxes::AdminRunner.new }
 
   describe '#ingest' do
+    before do
+      allow(Boxes).to receive(:bunny).and_return(double('bunny').as_null_object)
+    end
+
     it 'should create an order with its argument' do
-      order = double('order', to_json: 'something')
-      expect(Scalpel::Order).to receive(:new).with('image', 4, 5).and_return(order)
+      expect(Scalpel::Order).to receive(:new).with('image', 4, 5)
 
       runner.ingest 'image', '4', '5'
     end
 
-    it 'should make send the order json to the boxes.uncut message queue'
     it 'should connect to rabbitmq server with :mq_url option' do
       expect(Boxes).to receive(:bunny).with('butthole')
 
