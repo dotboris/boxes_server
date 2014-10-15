4148f99d6a12b9d208b1519cdace256c986eacf8
Boris Bera <bboris@rsoft.ca>
2014-10-15 18:05:39 -0400

    fixed tests for admin so that they make more sence

--- admin/spec/admin_runner_spec.rb
+++ admin/spec/admin_runner_spec.rb
@@ -5,10 +5,8 @@ describe Boxes::AdminRunner do
   let(:runner) { Boxes::AdminRunner.new }
 
   describe '#ingest' do
-    it 'should put the base64 encoded file in the request json'
-    it 'should make send an order to boxes.uncut'
-    it 'should include rows in json'
-    it 'should include columns in json'
+    it 'should create an order with its argument'
+    it 'should make send the order json to the boxes.uncut message queue'
     it 'should connect to rabbitmq server with :mq_url option' do
       expect(Boxes).to receive(:bunny).with('butthole')
 
