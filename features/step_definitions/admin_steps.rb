f900ea8479669a4bfcaedb0692ae3606351905e1
Boris Bera <bboris@rsoft.ca>
2014-10-19 19:23:16 -0400

    extracted common command running to a method

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -1,16 +1,8 @@
 require 'open3'
 
 def run_boxes_ctl(command, *args)
-  Bundler.with_clean_env do
-    inject_test_env!
-    Dir.chdir 'admin' do
-      cmd = %w{ruby -r bundler/setup ./bin/boxesctl} + [command, '--mq_url', amqp_url] + args
-      Open3.popen2e(*cmd) do |_, out, waiter|
-        status = waiter.value
-        raise "boxesctl failed with status code #{status.exitstatus} output:\n#{out.read}" unless status.success?
-      end
-    end
-  end
+  cmd = %w{ruby -r bundler/setup ./bin/boxesctl} + [command, '--mq_url', amqp_url] + args
+  run_in_dir('admin', *cmd)
 end
 
 When(/ingest "([^"]*)" cut in (\d+)x(\d+)/) do |file, rows, cols|
--- features/support/daemons.rb
+++ features/support/daemons.rb
@@ -1,5 +1,17 @@
 require 'tmpdir'
 
+def run_in_dir(dir, *cmd)
+  Bundler.with_clean_env do
+    inject_test_env!
+    Dir.chdir dir do
+      Open3.popen2e(*cmd) do |_, out, waiter|
+        status = waiter.value
+        raise "#{cmd.join(' ')} failed with status #{status.exitstatus} output:\n#{out.read}" unless status.success?
+      end
+    end
+  end
+end
+
 def amqp_url
   ENV['BOXES_AMQP_URL'] || 'amqp://boxes:boxes@localhost'
 end
