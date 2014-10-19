0ef68242b1375f23399f750bbf37323da80d246b
Boris Bera <bboris@rsoft.ca>
2014-10-19 13:05:27 -0400

    pop waiting is now polling with a sleep
    It's not as clean as the blocking method, but it does not required us to fight with bunny's threads and implementation.

--- drivethrough/lib/drivethrough/queue.rb
+++ drivethrough/lib/drivethrough/queue.rb
@@ -19,48 +19,13 @@ class DriveThrough
     end
 
     def pop_waiting(timeout)
-      done = false
-      received_message = false
-      race_mutex = Mutex.new
-
-      pop_mutex = Mutex.new
-      next_value = ConditionVariable.new
-
-      pop_mutex.synchronize do
-        ret = nil
-        consumer = Bunny::Consumer.new @queue.channel, @queue, '', false
-        consumer.on_delivery do |delivery_info, _, body|
-          race_mutex.synchronize do
-            unless received_message
-              # only run once
-              received_message = true
-              consumer.cancel
-
-              if done
-                @queue.channel.nack(delivery_info.delivery_tag, true, true)
-              else
-                ret = body
-                done = true
-                @queue.channel.acknowledge(delivery_info.delivery_tag, false)
-                next_value.signal
-              end
-              @queue.channel.nack(delivery_info.delivery_tag, true, true)
-            end
-          end
-        end
-
-        @queue.subscribe_with consumer, manual_ack: true
-
-        next_value.wait pop_mutex, timeout
-        race_mutex.synchronize do
-          if done
-            ret
-          else
-            done = true
-            raise TimeoutError
-          end
-        end
+      (timeout * 2).times do
+        res = @queue.pop
+        return res unless res.any? &:nil?
+        sleep 0.5
       end
+
+      raise TimeoutError, 'Could not get a message in time'
     end
   end
 end
