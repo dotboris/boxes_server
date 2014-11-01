879808a233a8f28f63bea54fa75797aa5a1570b7
Boris Bera <bboris@rsoft.ca>
2014-11-01 17:46:31 -0400

    request now has a col_count

--- commons/lib/gluegun/request.rb
+++ commons/lib/gluegun/request.rb
@@ -1,10 +1,10 @@
 require 'json'
 
 module GlueGun
-  Request = Struct.new(:queue, :row_count, :width, :height) do
+  Request = Struct.new(:queue, :row_count, :col_count, :width, :height) do
     def self.from_json(json)
       hash = JSON.parse json
-      return new(hash['queue'], hash['row_count'], hash['width'], hash['height'])
+      return new(hash['queue'], hash['row_count'], hash['col_count'], hash['width'], hash['height'])
     end
   end
 end
--- commons/spec/gluegun/request_spec.rb
+++ commons/spec/gluegun/request_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'gluegun/request'
 
 describe GlueGun::Request do
-  let(:request) { GlueGun::Request.new 'a.queue', 42, 200, 300 }
+  let(:request) { GlueGun::Request.new 'a.queue', 42, 55, 200, 300 }
 
   it 'should have a queue' do
     expect(request).to respond_to :queue, :queue=
@@ -12,6 +12,11 @@ describe GlueGun::Request do
   it 'should have a row count' do
     expect(request).to respond_to :row_count, :row_count=
     expect(request.row_count).to eq 42
+  end
+
+  it 'should have a col count' do
+    expect(request).to respond_to :col_count, :col_count=
+    expect(request.col_count).to eq 55
   end
 
   it 'should have a width' do
@@ -31,12 +36,13 @@ describe GlueGun::Request do
     end
 
     it 'should parse json' do
-      json = '{"queue": "boxes.collages.asdfg", "row_count": 2, "width": 100, "height": 400}'
+      json = '{"queue": "boxes.collages.asdfg", "row_count": 2, "col_count": 3, "width": 100, "height": 400}'
 
       request = GlueGun::Request.from_json(json)
 
       expect(request.queue).to eq 'boxes.collages.asdfg'
       expect(request.row_count).to eq 2
+      expect(request.col_count).to eq 3
       expect(request.width).to eq 100
       expect(request.height).to eq 400
     end
