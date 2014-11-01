1fdd390acaff78d4a405afea48a583c1d99f0cc0
Boris Bera <bboris@rsoft.ca>
2014-11-01 10:41:38 -0400

    trying to remove warning from travis when running rabbitmqctl

--- ci/rabbitmq_setup.sh
+++ ci/rabbitmq_setup.sh
@@ -2,7 +2,7 @@
 # sets up rabbit mq in travis ci environment
 # heavily inspired from https://github.com/ruby-amqp/bunny/blob/master/bin/ci/before_build.sh
 
-${RABBITMQ_NODENAME:="rabbit"}
+${RABBITMQ_NODENAME:="rabbit@travis"}
 ${RABBITMQCTL:="sudo rabbitmqctl -n $RABBITMQ_NODENAME"}
 
 # boxes:boxes has full access to /
