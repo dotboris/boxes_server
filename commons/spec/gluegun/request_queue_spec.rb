183bbdf7ff8e8ed509b22549fbcc134e97fe5a16
Boris Bera <bboris@rsoft.ca>
2014-11-01 14:16:59 -0400

    added integration tests for request_queue

---
+++ commons/spec/gluegun/request_queue_spec.rb
@@ -0,0 +1,52 @@
+require 'gluegun/request_queue'
+require 'bunny'
+
+describe Gluegun::RequestQueue do
+  before do
+    @connection = Bunny.new 'amqp://boxes:boxes@localhost'
+    @connection.start
+  end
+
+  after do
+    @connection.close
+  end
+
+  let(:queue) { Gluegun::RequestQueue.new @connection }
+
+  describe '#subscribe' do
+    # block that will successfully exit out of the blocking subscribe call
+    let(:block) do
+      proc do
+        # Fiddle with the insides of the request queue to be able to exit out of the blocking subscribe
+        # This makes the test very dependant on the internal implementation of the queue
+        inner_channel = queue.instance_variable_get(:@channel)
+        inner_channel.close
+      end
+    end
+
+    before do
+      channel = @connection.channel
+      channel.queue_purge 'boxes.collages'
+      queue = channel.queue 'boxes.collages'
+      queue.publish '{"some": "json"}'
+
+      # make sure we call the body of block
+      allow(block).to receive(:call).and_call_original
+    end
+
+    it 'should yield a response', timeout: 5 do
+      expect(block).to receive(:call).with(anything, kind_of(Boxes::QueueResponse))
+
+      queue.subscribe &block
+    end
+
+    it 'should yield a request', timeout: 5 do
+      request = double 'request'
+      allow(Gluegun::Request).to receive(:from_json).and_return(request)
+
+      expect(block).to receive(:call).with(request, anything)
+
+      queue.subscribe &block
+    end
+  end
+end
--- commons/spec/spec_helper.rb
+++ commons/spec/spec_helper.rb
@@ -1,3 +1,15 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'boxes'
 require 'fakefs/spec_helpers'
+
+RSpec.configure do |c|
+  c.around(:example) do |example|
+    if example.metadata[:timeout]
+      Timeout.timeout(example.metadata[:timeout]) do
+        example.run
+      end
+    else
+      example.run
+    end
+  end
+end
