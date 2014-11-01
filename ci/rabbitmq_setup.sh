97232eae0d777c0b32cb397ded65534401112c74
Boris Bera <bboris@rsoft.ca>
2014-11-01 11:41:39 -0400

    fixed odd travis bug
    The problem comes from the ${VAR:=butts} lines. Since they're the only lines, bash executes them. I changed them to use a normal if.

--- ci/rabbitmq_setup.sh
+++ ci/rabbitmq_setup.sh
@@ -2,7 +2,8 @@
 # sets up rabbit mq in travis ci environment
 # heavily inspired from https://github.com/ruby-amqp/bunny/blob/master/bin/ci/before_build.sh
 
-${RABBITMQCTL:="sudo rabbitmqctl"}
+[[ -z "${RABBITMQ_NODENAME:-}" ]] && RABBITMQ_NODENAME="rabbit"
+[[ -z "${RABBITMQCTL:-}" ]] && RABBITMQCTL="sudo rabbitmqctl -n ${RABBITMQ_NODENAME}"
 
 # boxes:boxes has full access to /
 ${RABBITMQCTL} add_vhost /
