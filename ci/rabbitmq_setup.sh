534b5ee678e076b2fafa4d59b82a92ee6e2ee384
Boris Bera <bboris@rsoft.ca>
2014-11-01 11:47:20 -0400

    made the travis rabbitmq script compatible with sh
    The script is a sh and not bash script. This means that [[ ...  ]] conditions don't work.

--- ci/rabbitmq_setup.sh
+++ ci/rabbitmq_setup.sh
@@ -2,8 +2,8 @@
 # sets up rabbit mq in travis ci environment
 # heavily inspired from https://github.com/ruby-amqp/bunny/blob/master/bin/ci/before_build.sh
 
-[[ -z "${RABBITMQ_NODENAME:-}" ]] && RABBITMQ_NODENAME="rabbit"
-[[ -z "${RABBITMQCTL:-}" ]] && RABBITMQCTL="sudo rabbitmqctl -n ${RABBITMQ_NODENAME}"
+[ -z "${RABBITMQ_NODENAME:-}" ] && RABBITMQ_NODENAME="rabbit"
+[ -z "${RABBITMQCTL:-}" ] && RABBITMQCTL="sudo rabbitmqctl -n ${RABBITMQ_NODENAME}"
 
 # boxes:boxes has full access to /
 ${RABBITMQCTL} add_vhost /
