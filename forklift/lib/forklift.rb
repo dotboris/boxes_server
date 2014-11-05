9fd5cf40a9d89fc91e8bedbc5d56ee347247c57a
Boris Bera <bboris@rsoft.ca>
2014-11-05 18:32:19 -0500

    forklift now requests for collage

--- forklift/lib/forklift.rb
+++ forklift/lib/forklift.rb
@@ -1,17 +1,28 @@
 require 'forklift/version'
 require 'boxes/split_image'
+require 'gluegun/request'
 
 class Forklift
-  def initialize(media_root, slices_queue)
+  def initialize(media_root, slices_queue, request_queue)
     @media_root = media_root
     @slices_queue = slices_queue
+    @request_queue = request_queue
   end
 
   def load_slices
     split_image = Boxes::SplitImage.pick_active @media_root
     puts "the winner is (drum roll)... #{split_image.inspect}"
 
+    request = GlueGun::Request.new SecureRandom.uuid,
+                                   split_image.row_count,
+                                   split_image.slices.size / split_image.row_count,
+                                   split_image.width,
+                                   split_image.height
+    @request_queue.publish request
+    puts 'Sent a collage request'
+
     split_image.slices.each { |slice| @slices_queue.publish slice }
+
     puts 'Published slices'
   end
 end
--- forklift/spec/forklift_spec.rb
+++ forklift/spec/forklift_spec.rb
@@ -10,20 +10,32 @@ describe Forklift do
 
     let(:media_root) { double 'media root' }
     let(:slices_queue) { double 'slices queue' }
-    let(:forklift) { Forklift.new media_root, slices_queue }
+    let(:request_queue) { double 'request queue' }
+    let(:forklift) { Forklift.new media_root, slices_queue, request_queue }
+
+    let(:original) { double 'original image' }
+    let(:split_image) do
+      double 'split image',
+             slices: ['thing'],
+             row_count: 1,
+             width: 100,
+             height: 100
+    end
+
+    before do
+      allow(Boxes::SplitImage).to receive(:pick_active).and_return split_image
+      allow(slices_queue).to receive(:publish)
+      allow(request_queue).to receive(:publish)
+    end
 
     it 'should pick a random sliced image' do
-      forklift = Forklift.new media_root, slices_queue
-
-      expect(Boxes::SplitImage).to receive(:pick_active).with(media_root).
-                                       and_return(double('sliced image').as_null_object)
+      expect(Boxes::SplitImage).to receive(:pick_active).with(media_root)
 
       forklift.load_slices
     end
 
     it 'should publish all slices' do
-      split_image = double('split image', slices: %w{echo foxtrot golf hotel})
-      allow(Boxes::SplitImage).to receive(:pick_active).and_return(split_image)
+      allow(split_image).to receive(:slices).and_return %w{echo foxtrot golf hotel}
 
       expect(slices_queue).to receive(:publish).with('echo')
       expect(slices_queue).to receive(:publish).with('foxtrot')
@@ -33,6 +45,21 @@ describe Forklift do
       forklift.load_slices
     end
 
-    it 'should create a collage request'
+    it 'should create a collage request' do
+      allow(split_image).to receive(:slices).and_return Array.new(18)
+      allow(split_image).to receive(:row_count).and_return 6
+      allow(split_image).to receive(:width).and_return 900
+      allow(split_image).to receive(:height).and_return 100
+
+      expect(request_queue).to receive(:publish) do |request|
+        expect(request.queue).not_to be_nil
+        expect(request.row_count).to eq 6
+        expect(request.col_count).to eq 3
+        expect(request.width).to eq 900
+        expect(request.height).to eq 100
+      end
+
+      forklift.load_slices
+    end
   end
 end
