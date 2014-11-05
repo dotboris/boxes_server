1b692482f39c82181cc1a119db426482f673c578
Boris Bera <bboris@rsoft.ca>
2014-11-05 14:07:52 -0500

    request handler now uses mosaic maker

--- gluegun/lib/gluegun/request_handler.rb
+++ gluegun/lib/gluegun/request_handler.rb
@@ -1,5 +1,6 @@
 require 'gluegun/drawing_queue'
 require 'gluegun/collector'
+require 'gluegun/mosaic_maker'
 
 module GlueGun
   class RequestHandler
@@ -10,12 +11,20 @@ module GlueGun
     def call(request, response)
       puts "Got request: #{request.inspect}"
 
-      collect_drawings(request)
+      drawings = collect_drawings(request)
+
+      puts 'Gluing drawings together'
+      collage = glue_drawings request, drawings
+      puts "Collage is #{collage}"
 
       response.ack
     end
 
     private
+
+    def glue_drawings(request, drawings)
+      GlueGun::MosaicMaker.new(request.col_count, request.row_count, request.width, request.height).call(drawings)
+    end
 
     def collect_drawings(request)
       drawings_queue = GlueGun::DrawingQueue.new @connection, request.queue
--- gluegun/spec/request_handler_spec.rb
+++ gluegun/spec/request_handler_spec.rb
@@ -1,17 +1,37 @@
 require 'gluegun/request'
 require 'gluegun/request_handler'
+require 'gluegun/mosaic_maker'
 
 describe GlueGun::RequestHandler do
   let(:connection) { double 'connection' }
   let(:handler) { GlueGun::RequestHandler.new connection }
 
+  let(:collector) { double 'collector', call: []}
+  let(:queue) { double 'queue' }
+  let(:maker) { double 'mosaic maker', call: nil}
+
+  before do
+    allow(GlueGun::DrawingQueue).to receive(:new).and_return(queue)
+    allow(GlueGun::Collector).to receive(:new).and_return(collector)
+    allow(GlueGun::MosaicMaker).to receive(:new).and_return(maker)
+  end
+
   it 'should collect drawings', :noisy do
     request = GlueGun::Request.new 'the.prettiest.little.queue', 3, 2
-    queue = double 'queue'
-    collector = double 'collector', call: ['fi', nil, 'fum']
+    allow(collector).to receive(:call).and_return(['fi', nil, 'fum'])
 
-    expect(GlueGun::DrawingQueue).to receive(:new).with(connection, 'the.prettiest.little.queue').and_return(queue)
-    expect(GlueGun::Collector).to receive(:new).with(queue, 6).and_return(collector)
+    expect(GlueGun::DrawingQueue).to receive(:new).with(connection, 'the.prettiest.little.queue')
+    expect(GlueGun::Collector).to receive(:new).with(queue, 6)
+
+    handler.call(request, double.as_null_object)
+  end
+
+  it 'should build a mosaic with the image' do
+    request = GlueGun::Request.new nil, 2, 3, 50, 100
+    allow(collector).to receive(:call).and_return(%w{a b c d e f})
+
+    expect(GlueGun::MosaicMaker).to receive(:new).with(3, 2, 50, 100)
+    expect(maker).to receive(:call).with(%w(a b c d e f))
 
     handler.call(request, double.as_null_object)
   end
